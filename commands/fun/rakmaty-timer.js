c19138d352e9864144083ce18a07f07f79b33ed6
KrykyX <84568503+KrykyX@users.noreply.github.com>
2025-09-06 23:15:57 +0200

    timer: add command, update index file

---
+++ commands/fun/rakmaty-timer.js
@@ -0,0 +1,31 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction, ChannelType } from 'discord.js';
+import { sep } from 'path';
+import { secondsToString } from '../../global.js';
+
+const commandName = import.meta.url.split(sep).pop().slice(0, import.meta.url.split(sep).pop().length - 3);
+
+export default {
+	guild: false,
+	data: new SlashCommandBuilder()
+		.setName(commandName)
+		.setDescription('zjisti jak dlouho trvalo rakmatymu se připojit do tohoto threadu'),
+
+	/**
+	 * Execute the command
+	 *
+	 * @param {ChatInputCommandInteraction} interaction
+	 */
+	async execute(interaction) {
+		await interaction.deferReply();
+
+		if (interaction.channel.type != ChannelType.PublicThread && interaction.channel.type != ChannelType.PrivateThread) throw new Error('peepo: kanál není thread');
+
+		const rakmaty1 = await interaction.channel.members.fetch(process.env.RAKMATY_ID).catch(() => {
+			throw new Error('peepo: rakmaty1 není v tomto threadu');
+		});
+
+		const dateBits = Number(BigInt.asUintN(64, interaction.channel.id) >> 22n);
+
+		await interaction.editReply('✅ rakmaty se připojil ' + secondsToString(Math.floor((rakmaty1.joinedTimestamp - (dateBits + 1420070400000)) / 1000)) + ' po založení threadu');
+	},
+};
