cd9a5aca43b353a94d7d996f0b151fc894b81747
KrykyX <84568503+KrykyX@users.noreply.github.com>
2025-09-07 23:17:00 +0200

    survey: fix and improve command

--- commands/fun/rakmaty-survey.js
+++ commands/fun/rakmaty-survey.js
@@ -17,44 +17,73 @@ export default {
 	 * @param {ChatInputCommandInteraction} interaction
 	 */
 	async execute(interaction) {
-		await interaction.deferReply({
-			flags: MessageFlags.Ephemeral,
-		});
+		await interaction.reply('⏩ začal jsem hledat...');
 
 		const guilds = await interaction.client.guilds.fetch();
 
-		const times = [];
+		let times = [];
+		let countAll = 0, countRakmaty = 0;
 
-		for (const guildPartial of guilds) {
-			const guild = await guildPartial[1].fetch();
+		for (const [guildPartialId, guildPartial] of guilds) {
+			const guild = await guildPartial.fetch();
 			const channels = await guild.channels.fetch();
 
-			for (const channel of channels) {
-				if (!channel[1].threads) continue;
+			for (const [channelId, channel] of channels) {
+				if (!channel.threads) continue;
 
-				const threadsFetched = await channel[1].threads.fetch();
-				const threads = threadsFetched.threads;
+				const threadsFetchedActive = await channel.threads.fetchActive();
+
+
+				let threadsFetchedArchivedPublic, threadsFetchedArchivedPrivate;
+
+				try {
+					threadsFetchedArchivedPublic = await channel.threads.fetchArchived({
+						type: 'public',
+						fetchAll: true,
+					});
+				}
+				// eslint-disable-next-line no-empty
+				catch {}
+
+				try {
+					threadsFetchedArchivedPrivate = await channel.threads.fetchArchived({
+						type: 'private',
+						fetchAll: true,
+					});
+				}
+				// eslint-disable-next-line no-empty
+				catch {}
+
+				let threads = threadsFetchedActive.threads;
+
+				if (threadsFetchedArchivedPublic) threads = threads.concat(threadsFetchedArchivedPublic.threads);
+				if (threadsFetchedArchivedPrivate) threads = threads.concat(threadsFetchedArchivedPrivate.threads);
 
 				if (threads.size === 0) continue;
 
-				for (const thread of threads) {
-					let rakmaty1;
+				for (const [threadId, thread ] of threads) {
+					countAll++;
+					if (countAll % 30 == 0 && countAll != 0) await interaction.editReply(`⏩ prošel jsem už ${countAll} threadů, v ${countRakmaty} z nich byl rakmaty, hledám dál...`);
 
+					let rakmaty1;
 					try {
-						rakmaty1 = await thread[1].members.fetch(process.env.RAKMATY_ID);
+						rakmaty1 = await thread.members.fetch(process.env.RAKMATY_ID);
 					}
 					catch {
 						continue;
 					}
 
-					const dateBits = Number(BigInt.asUintN(64, thread[1].id) >> 22n);
+					countRakmaty++;
 
-					console.log(Math.floor((rakmaty1.joinedTimestamp - (dateBits + 1420070400000)) / 1000));
+					const seconds = Math.floor((rakmaty1.joinedTimestamp - (Number(BigInt.asUintN(64, thread.id) >> 22n) + 1420070400000)) / 1000);
 
-					times.push(Math.floor((rakmaty1.joinedTimestamp - (dateBits + 1420070400000)) / 1000));
+					if (thread.ownerId != process.env.RAKMATY_ID && seconds > 1) times.push(seconds);
 				}
 			}
 		}
+
+		if (countRakmaty === 0) throw new Error(`peepo: v žádném z threadů do kterých mám přístup (celkem ${countAll}) není rakmaty`);
+		if (times.length === 0) throw new Error(`peepo: prošel jsem všechny thready do kterých mám přístup (celkem ${countAll}) a je v nich rakmaty (celkem ${countRakmaty}), ale žádný nevyhovuje podmínkám pro statistiky (thready založené rakmatym se nepočítají, stejně tak thready do kterých byl přidaný první zprávou).`);
 
 		let min = times[0], max = times[0], sum = times[0];
 
@@ -66,6 +95,11 @@ export default {
 
 		const avg = sum / times.length;
 
-		await interaction.editReply(`✅ prošel jsem všechny thready do kterých mám přístup, tady jsou rakmatyho statistiky připojení:\nmin: ${secondsToString(min)}\nmax: ${secondsToString(max)}\navg: ${secondsToString(avg)}`);
+		times = [...times].sort((a, b) => a - b);
+		const halfpoint = Math.floor(times.length / 2);
+
+		const median = (times.length % 2 ? times[halfpoint] : (times[halfpoint - 1] + times[halfpoint]) / 2);
+
+		await interaction.editReply(`✅ prošel jsem všechny thready do kterých mám přístup (celkem ${countAll}) a je v nich rakmaty (celkem ${countRakmaty}), tady jsou jeho statistiky připojení (thready založené rakmatym se nepočítají, stejně tak thready do kterých byl přidaný první zprávou):\n\nmin: ${secondsToString(min)}\nmax: ${secondsToString(max)}\n\nprůměr: ${secondsToString(Math.round(avg))}\nmedián: ${secondsToString(median)}`);
 	},
 };
