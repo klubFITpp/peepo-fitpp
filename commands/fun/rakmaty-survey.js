2c45db5104cbac76da5f5a3883fd8fb8c2bc6006
Marek Vospel <73162071+marekvospel@users.noreply.github.com>
2026-02-16 21:51:27 +0100

    fix(rakmaty-survey): use the threads from fetch threads (#2)

--- commands/fun/rakmaty-survey.js
+++ commands/fun/rakmaty-survey.js
@@ -44,7 +44,7 @@ export default {
 			];
 
 			return (await Promise.allSettled(promises))
-				.filter(res => res.status === 'fulfilled').flatMap(res => res.value);
+				.filter(res => res.status === 'fulfilled').flatMap(res => res.value.threads);
 		}))).filter(Boolean).flat()
 
 
